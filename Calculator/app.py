229be50276e28aac98546eeb8b264c74d8b42c8b
Carl Pereira <137459719+LoneEntity@users.noreply.github.com>
2023-06-30 01:11:17 +0100

    Add files via upload

---
+++ Calculator/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        result = num1 / num2
+
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run()
